147e2e35fd9a3ae194bd0b9e169a5f7cbd7aef8e
Cliente CONTROL-PC <CltControl@GGIECUT1WRK040.espol.edu.ec>
2018-07-19 12:55:45 -0500

    xd

---
+++ Patrones2/src/ConPatrones/FabricaAles.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConPatrones;
+
+import SinPatrones.Articulo;
+
+/**
+ *
+ * @author CltControl
+ */
+public class FabricaAles implements FabricaLocales {
+
+    public FabricaAles(){
+        
+    }
+    
+    @Override
+    public Articulo fabricarPolloAsado() {
+        Articulo articulo = new Articulo();
+	return articulo;
+    }
+
+    @Override
+    public Articulo fabricarArrozConMenestra() {
+        Articulo articulo = new Articulo();
+	return articulo;
+    }
+    
+    
+}
---
+++ Patrones2/src/ConPatrones/FabricaGus.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConPatrones;
+
+import SinPatrones.Articulo;
+import SinPatrones.PollosGus;
+
+/**
+ *
+ * @author CltControl
+ */
+public class FabricaGus implements FabricaLocales{
+    
+    public FabricaGus(){
+        
+    }
+
+    @Override
+    public Articulo fabricarPolloAsado() {
+        Articulo articulo = new Articulo();
+        return articulo;
+    }    
+
+    @Override
+    public Articulo fabricarArrozConMenestra() {
+        Articulo articulo = new Articulo();
+	return articulo;
+    }
+    
+  
+}
